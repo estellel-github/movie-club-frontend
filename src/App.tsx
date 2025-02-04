9b214a03c02103d357713314dbde3945fd12bd97
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-02-04 01:44:59 +0100

    Fix prettier config, apply mass formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 import AppRouter from "./router";
-import "./index.css"; 
+import "./index.css";
 
 function App() {
   return <AppRouter />;
