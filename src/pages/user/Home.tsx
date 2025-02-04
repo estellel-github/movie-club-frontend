b49bc0a1951c1f7f2b42e0c1aa8e987e28499f4e
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-02-04 15:56:47 +0100

    Add placeholder content for Home page

--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,5 +1,15 @@
-const Home = () => {
-  return <div></div>;
-};
+import Navbar from "../../components/layout/navBar";
 
-export default Home;
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold">Welcome to Movie Club 🎬</h1>
+        <p className="text-gray-600">
+          Your activity feed will be displayed here.
+        </p>
+      </div>
+    </div>
+  );
+}
