81e6ebd96f201e3d1bd48fb217a42934c5d448d3
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-02-10 16:29:44 +0100

    Fix public header

--- src/components/layout/PublicHeader.tsx
+++ src/components/layout/PublicHeader.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="navbar bg-neutral-950 text-neutral-200 px-6 border-b-2 border-b-stone-800 ">
+    <div className="navbar bg-neutral-950 text-neutral-200 px-6 border-b-2 border-b-stone-800 justify-between">
       {/* Left (Logo & Brand) */}
       <div className="navbar-start">
         <Link to="/" className="flex items-center gap-3">
           <img src={"/images/bmc_logo.png"} className="h-8 w-auto" alt="Logo" />
-          <span className="text-xl font-bold text-yellow-400">
+          <span className="text-xl font-bold text-yellow-400 whitespace-nowrap">
             Berlin Movie Club
           </span>
         </Link>
@@ -41,27 +41,25 @@ export default function Header() {
       {/* Mobile Dropdown */}
       <div className="navbar-end lg:hidden w-64">
         <div className="dropdown w-64">
-          <div className="flex justify-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
+          <div tabIndex={0} role="button" className="flex  justify-end">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content w-full bg-neutral-950 text-neutral-200 rounded-box z-[1] mt-3 p-2 shadow"
+            className="menu menu-sm dropdown-content w-full bg-neutral-950 text-neutral-200 rounded-box z-[1] mt-3 p-2 shadow -top-6"
           >
             <li>
               <Link to="/events-public" className="text-2xl mb-4">
