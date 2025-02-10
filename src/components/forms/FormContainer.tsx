cf9fe50a40dd8801cc7a929e093ec6f4b9ddd9ad
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-02-10 12:37:53 +0100

    Adjust bottom margin for form

--- src/components/forms/FormContainer.tsx
+++ src/components/forms/FormContainer.tsx
@@ -27,7 +27,7 @@ export default function FormContainer({
 }: FormContainerProps) {
   return (
     <div className="flex flex-col m-auto">
-      <div className="min-w-[412px] sm:min-w-[512px] max-w-md mx-auto mt-8 p-6 bg-stone-800 rounded-lg shadow-lg">
+      <div className="min-w-[412px] sm:min-w-[512px] max-w-md mx-auto mt-8 mb-8 p-6 bg-stone-800 rounded-lg shadow-lg">
         <h2 className="text-center mb-4">{title}</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           {fields.map((field) => (
