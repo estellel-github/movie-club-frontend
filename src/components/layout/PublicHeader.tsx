893fda18eace8e406e231b92a45d699fa2ba778e
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-02-10 15:49:25 +0100

    Change public header to responsive w hamburger

--- src/components/layout/PublicHeader.tsx
+++ src/components/layout/PublicHeader.tsx
@@ -2,35 +2,89 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="navbar flex items-center justify-between bg-neutral-950 text-neutral-200 px-6 border-b-2 border-b-stone-800">
-      <Link to="/" className="flex items-center gap-3">
-        <img src={"/images/bmc_logo.png"}></img>
-        <div className="text-2xl font-bold text-yellow-400">
-          Berlin Movie Club
-        </div>
-      </Link>
-      <div className="flex gap-4">
-        <Link
-          to="/events-public"
-          className="text-lg mx-4 hover:text-yellow-400"
-        >
-          Events
+    <div className="navbar bg-neutral-950 text-neutral-200 px-6 border-b-2 border-b-stone-800 ">
+      {/* Left (Logo & Brand) */}
+      <div className="navbar-start">
+        <Link to="/" className="flex items-center gap-3">
+          <img src={"/images/bmc_logo.png"} className="h-8 w-auto" alt="Logo" />
+          <span className="text-xl font-bold text-yellow-400">
+            Berlin Movie Club
+          </span>
         </Link>
-        <Link to="/contact" className="text-lg mx-4 hover:text-yellow-400">
-          Contact
-        </Link>
-        <Link
-          to="/signup"
-          className="text-lg mx-4 hover:text-yellow-400 neutral-200space-nowrap"
-        >
+      </div>
+
+      {/* Center (Navigation for larger screens) */}
+      <div className="navbar-center hidden lg:flex"></div>
+
+      {/* Right (Auth Links) */}
+      <div className="navbar-end hidden lg:flex gap-4">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/events-public" className="hover:text-yellow-400 text-xl">
+              Events
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-yellow-400 text-xl">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <Link to="/signup" className="btn btn-sm btn-outline text-white">
           Sign Up
         </Link>
-        <Link
-          to="/login"
-          className="text-lg mx-4 hover:text-yellow-400 neutral-200space-nowrap"
-        >
+        <Link to="/login" className="btn btn-sm btn-warning">
           Log In
         </Link>
+      </div>
+
+      {/* Mobile Dropdown */}
+      <div className="navbar-end lg:hidden w-64">
+        <div className="dropdown w-64">
+          <div className="flex justify-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content w-full bg-neutral-950 text-neutral-200 rounded-box z-[1] mt-3 p-2 shadow"
+          >
+            <li>
+              <Link to="/events-public" className="text-2xl mb-4">
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="text-2xl mb-4">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup" className="text-2xl mb-4">
+                Sign Up
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className="text-2xl mb-4">
+                Log In
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
