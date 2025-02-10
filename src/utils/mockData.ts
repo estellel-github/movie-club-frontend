5bb6b47298df7c75003eb1ae89f941113cf17cf9
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-02-10 12:27:16 +0100

    Move event mock data to utils

---
+++ src/utils/mockData.ts
@@ -0,0 +1,59 @@
+export const upcomingEvents = [
+  {
+    id: "1",
+    title: "February Movie #1 - Babygirl",
+    date: "Feb. 15 2025 - 19:00",
+    image: "/images/event1.jpg",
+  },
+  {
+    id: "2",
+    title: "February Movie #2 - Wicked",
+    date: "Feb. 22 2025 - 19:00",
+    image: "/images/event2.jpg",
+  },
+  {
+    id: "3",
+    title: "February Movie #3 - The Brutalist",
+    date: "Feb. 28 2025 - 19:00",
+    image: "/images/event3.jpg",
+  },
+];
+
+export const pastEvents = [
+  {
+    id: "4",
+    title: "January Movie #3 - The Outrun",
+    date: "Jan. 28 2025 - 19:00",
+    image: "/images/event4.jpg",
+  },
+  {
+    id: "5",
+    title: "January Movie #2 - Queer",
+    date: "Jan. 22 2025 - 19:00",
+    image: "/images/event5.jpg",
+  },
+  {
+    id: "6",
+    title: "January Movie #1 - Nosferatu",
+    date: "Jan. 15 2025 - 19:00",
+    image: "/images/event6.jpg",
+  },
+  {
+    id: "7",
+    title: "December Movie #3 - The Last of the Sea Women",
+    date: "Dec. 20 2024 - 19:00",
+    image: "/images/event7.jpg",
+  },
+  {
+    id: "8",
+    title: "December Movie #2 - The Substance",
+    date: "Dec. 13 2024 - 19:00",
+    image: "/images/event8.jpg",
+  },
+  {
+    id: "9",
+    title: "December Movie #1 - The Wild Robot",
+    date: "Dec. 3 2024 - 19:00",
+    image: "/images/event9.jpg",
+  },
+];
